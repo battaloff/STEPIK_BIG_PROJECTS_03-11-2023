fb6168ed91941321fb4330bf3093a27e7b48ac3e
Ravshan <battaloff@mail.ru>
2023-11-03 17:36:45 +0500

    Третий проект КРУД Дополнение под вечер

--- Cource_ThirdProject/crud/models.py
+++ Cource_ThirdProject/crud/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=20)
     age = models.IntegerField()
+
+    def __str__(self):
+        return self.name[:20]
+
--- Cource_ThirdProject/crud/views.py
+++ Cource_ThirdProject/crud/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponseNotFound
+from .models import Person
 
-# Create your views here.
+
+def index(request):
+    people = Person.objects.all()
+    return render(request, 'index.html', {'people': people})
+
+
+def create(request):
+    if request.method == 'POST':
+        person = Person()
+        person.name = request.POST.get('name')
+        person.age = request.POST.get('age')
+        person.save()
+        return HttpResponseRedirect('/')
+
+
+def edit(request):
+    try:
+        person = Person.objects.get(id=id)
+
+        if request.method == 'POST':
+            person.name = request.POST.get('name')
+            person.age = request.POST.get('age')
+            person.save()
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, 'edit.html', {'person': person})
+    except Person.DoesNotExist:
+        return HttpResponseNotFound
